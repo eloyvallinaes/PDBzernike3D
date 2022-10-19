2fb890f4a2b9c7a81556fd6745e3e6d3882f0b56
evallina <eloyvallina33@gmail.com>
2022-10-19 18:07:27 +0200

    Aggresive smoothing and represent with pyvista

--- PDBzernike3D/potentialMap.py
+++ PDBzernike3D/potentialMap.py
@@ -6,54 +6,58 @@ https://ccsb.scripps.edu/msms/documentation/) which are run with subprocess.
 
 import trimesh
 import numpy as np
+import pyvista as pv
 from pathlib import Path
 from gridData import Grid
+import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from matplotlib.colors import TwoSlopeNorm
-import matplotlib.cm as cm
+from scipy.ndimage import gaussian_filter1d
 
 from PDBzernike3D.voxelise import load_vertices, load_faces, represent
 
 
 if __name__ == "__main__":
-    dir = Path("examples/trypsin_inh")
+    folder = Path("examples/trypsin_inh")
     code = "1tpa.I"
 
     # APBS electronic potential grid
-    grid = Grid(dir / f"potential.dx")
+    grid = Grid(folder / f"potential.dx")
     gridCoords = np.array(np.meshgrid(*grid.midpoints)).T.reshape(-1, 3)
     potential = trimesh.points.PointCloud(gridCoords)
 
     # protein surface mesh
-    vv = load_vertices(dir / f"{code}.vert")
-    ff = load_faces(dir / f"{code}.face")
+    vv = load_vertices(folder / f"{code}.vert")
+    ff = load_faces(folder / f"{code}.face")
     surface = trimesh.Trimesh(vertices=vv, faces=ff)
 
     #
     ww = np.arange(1, 21)[::-1]
-    _, potInd = potential.kdtree.query(surface.vertices, 20)
-    potValues = []
-    for ind in potInd:
-        potValues.append(
-            np.dot(
-                grid.grid[np.unravel_index(ind, grid.grid.shape)],
-                ww,
-            ) / ww.sum()
-        )
+    _, potInd = potential.kdtree.query(surface.vertices, 80)
+    potValues = {
+        tuple(vertex): grid.grid[np.unravel_index(ind, grid.grid.shape)].mean()
+        for vertex, ind in zip(surface.vertices, potInd)
+    }
+    # aggresively smooth over surface neighbouring vertices
+    smoothed = {}
+    for vertex, value in potValues.items():
+        _, verInd = surface.kdtree.query(vertex, 16)
+        verVals = [
+            potValues[tuple(vertex)]
+            for vertex in surface.vertices[verInd]
+        ]
+        smoothed[tuple(vertex)] = gaussian_filter1d(verVals, 2)[0]
+    # convert to colors
     norm = TwoSlopeNorm(vcenter=0, vmin=-1, vmax=1)
-    colors = cm.bwr_r(norm(potValues))
-    surface.visual.vertex_colors = colors
-    # surface.show()
-
-    #
-    # fig = plt.figure(figsize=(10, 10))
-    # ax = fig.add_subplot(111, projection="3d")
-    # ax.scatter(
-    #     surface.vertices[:, 0],
-    #     surface.vertices[:, 1],
-    #     surface.vertices[:, 2],
-    #     c=colors,
-    #     marker="."
-    # )
-    # plt.show()
+    normed = np.nan_to_num(
+        norm(
+            np.array(list(smoothed.values())).data
+        ),
+        copy=True,
+        posinf=1,
+        neginf=-1
+    )
+    cloud = pv.wrap(surface)
+    cloud['data'] = normed
+    # cloud.plot(cmap='bwr_r')
